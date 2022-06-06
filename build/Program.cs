aba8975fc93a53fa6d2fafdffa696544ac9d5553
Jordan anderson <mr@jordananderson.io>
2022-06-06 04:29:33 +0100

    Fix build script

--- build/Program.cs
+++ build/Program.cs
@@ -10,29 +10,29 @@ using Cake.Frosting;
 
 public static class Program
 {
-    public static int Main(string[] args)
-    {
-        return new CakeHost()
-            .UseContext<BuildContext>()
-            .Run(args);
-    }
+  public static int Main(string[] args)
+  {
+    return new CakeHost()
+        .UseContext<BuildContext>()
+        .Run(args);
+  }
 }
 
 public class BuildContext : FrostingContext
 {
-    public string SolutionPath { get; }
-    public string NugetApiKey { get; }
-    public string Version { get; }
-    public string AssemblyVersion { get; }
+  public string SolutionPath { get; }
+  public string NugetApiKey { get; }
+  public string Version { get; }
+  public string AssemblyVersion { get; }
 
-    public BuildContext(ICakeContext context)
-        : base(context)
-    {
-        SolutionPath = System.IO.Path.GetFullPath(context.Arguments.GetArgument("SolutionPath"));
-        NugetApiKey = context.Arguments.GetArgument("NugetApiKey");
-        Version = context.Arguments.GetArgument("BuildVersion");
-        AssemblyVersion = context.Arguments.GetArgument("AssemblyVersion");
-    }
+  public BuildContext(ICakeContext context)
+      : base(context)
+  {
+    SolutionPath = System.IO.Path.GetFullPath(context.Arguments.GetArgument("SolutionPath"));
+    NugetApiKey = context.Arguments.GetArgument("NugetApiKey");
+    Version = context.Arguments.GetArgument("BuildVersion");
+    AssemblyVersion = context.Arguments.GetArgument("AssemblyVersion");
+  }
 }
 
 [TaskName("Clean")]
@@ -43,8 +43,10 @@ public sealed class CleanTask : FrostingTask<BuildContext>
     var artifacts = System.IO.Path.Combine(context.SolutionPath, "artifacts/test-results");
     if (Directory.Exists(artifacts))
     {
-      Directory.Delete(artifacts);
+      Directory.Delete(artifacts, true);
     }
+
+    Directory.SetCurrentDirectory(context.SolutionPath);
   }
 }
 
@@ -65,7 +67,7 @@ public sealed class BuildTask : FrostingTask<BuildContext>
 {
   public override void Run(BuildContext context)
   {
-    context.DotNetBuild(context.SolutionPath, new DotNetBuildSettings
+    context.DotNetBuild(Directory.GetCurrentDirectory(), new DotNetBuildSettings
     {
       Configuration = "Release",
       NoRestore = true,
@@ -82,13 +84,13 @@ public sealed class TestTask : FrostingTask<BuildContext>
 {
   public override void Run(BuildContext context)
   {
-    context.DotNetTest(context.SolutionPath, new DotNetTestSettings
+    context.DotNetTest(Directory.GetCurrentDirectory(), new DotNetTestSettings
     {
       Configuration = "Release",
       NoRestore = true,
       NoBuild = true,
       ResultsDirectory = System.IO.Path.Combine(context.SolutionPath, "artifacts/test-results"),
-      Loggers = {"trx"}
+      Loggers = { "trx" }
     });
   }
 }
@@ -97,34 +99,41 @@ public sealed class TestTask : FrostingTask<BuildContext>
 [IsDependentOn(typeof(TestTask))]
 public sealed class PackTask : FrostingTask<BuildContext>
 {
-    public override void Run(BuildContext context)
+  public override void Run(BuildContext context)
+  {
+    context.DotNetPack(Directory.GetCurrentDirectory(), new DotNetCorePackSettings
     {
-        context.DotNetPack(context.SolutionPath, new DotNetCorePackSettings
-        {
-          Configuration = "Release",
-          NoRestore = true, 
-          NoBuild = true,
-          OutputDirectory = System.IO.Path.Combine(context.SolutionPath, "artifacts/packages"),
-          ArgumentCustomization = args => args
-            .Append($"/p:Version={context.Version}")
-        });
-    }
+      Configuration = "Release",
+      NoRestore = true,
+      NoBuild = true,
+      OutputDirectory = Path.Combine(context.SolutionPath, "artifacts/packages"),
+      ArgumentCustomization = args => args
+        .Append($"/p:Version={context.Version}")
+        .Append($"/p:PackageVersion={context.Version}")
+    });
+  }
 }
 
 [TaskName("Publish")]
 [IsDependentOn(typeof(PackTask))]
 public sealed class PublishTask : FrostingTask<BuildContext>
 {
-    // Tasks can be asynchronous
-    public void RunAsync(BuildContext context)
+  // Tasks can be asynchronous
+  public override void Run(BuildContext context)
+  {
+    var packages = Directory.EnumerateFiles(Path.Combine(Directory.GetCurrentDirectory(), "artifacts/packages/"), "*.nupkg");
+    var settings = new DotNetNuGetPushSettings
     {
-        context.DotNetNuGetPush(
-            System.IO.Path.Combine(context.SolutionPath, "artifacts/packages"), 
-            new DotNetNuGetPushSettings
-            {
-              ApiKey = context.NugetApiKey,
-            });
+      ApiKey = context.NugetApiKey,
+      Source = "https://api.nuget.org/v3/index.json"
+    };
+
+    foreach (var package in packages)
+    {
+      context.DotNetNuGetPush(package, settings);
     }
+
+  }
 }
 
 [TaskName("Default")]
